baa97b4f5e1381dcf3f0bba6204a738210d50e3c
nasimxon <nasimxon0908@gmail.com>
2017-05-27 20:03:04 +0500

    Initial commit

---
+++ app/src/main/java/com/example/root/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.root.myapplication;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}
